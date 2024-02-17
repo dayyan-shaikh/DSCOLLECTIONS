cb0710a6a3165094e2fafed1b4e67b7bc5ef9130
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-02-17 21:17:03 +0530

    commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import img from './bg.jpg'
+import Product from "./Product";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="card text-bg-dark border-0">
+        <img src={img} className="card-img" alt="background" height="550px"/>
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+            <div className="container">
+            <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
+            <p className="card-text lead fs-2">
+                CHECK OUT ALL THE TRENDS
+            </p>
+            </div>
+        </div>
+      </div>
+      <Product />
+    </div>
+  );
+};
+
+export default Home;
